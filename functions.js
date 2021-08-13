7e16af8549b6985c0f87023234adbe889dcbf930
danielebanovaz <daniele.banovaz@gmail.com>
2021-08-13 11:18:53 +0200

    Accessing individual characters in a string

--- functions.js
+++ functions.js
@@ -5,17 +5,17 @@ let userName = "Rafael"
 
 let greeting
 
-if (userName === "John") {
+/*if (userName === "John") {
     greeting = "Good morning, John!"
 } else if (userName === "Rafael") {
-    greeting = "Bom dia, Rafael!"
+    greeting = "Bom dia, Rafael!!!"
 } else if (userName === "Bogdan") {
     greeting = "Buna ziua, Bogdan!"
 } else if (userName === "Lorenzo") {
     greeting = "Buongiorno Lorenzo!"
 } else {
     greeting = "Hi " + userName
-}
+}*/
 
 switch(userName)
 {
@@ -26,5 +26,31 @@ switch(userName)
     default: greeting = "Hi " + userName; break
 }
 
-console.log(greeting)
+console.log("A new user joined.", greeting)
 
+
+console.log(">-------------< STRING MANIPULATION FUNCTIONS >-------------<")
+
+let helloStriversString = "Hello Strivers" // We can access individual characters as if it were an array
+                    //     0123456789   <-- these are the indexes
+
+console.log("The seventh character is", helloStriversString[6])
+
+let dilatedString = ""
+for (let i = 0; i < helloStriversString.length; i++) {
+    
+    let currentCharacter = helloStriversString[i]
+
+    if (currentCharacter === " ")
+        continue
+
+    if (currentCharacter === "i")
+        break
+
+    console.log(currentCharacter, " ")
+    dilatedString += currentCharacter + "   "
+}
+
+console.log(dilatedString)
+
+// H e l l o
